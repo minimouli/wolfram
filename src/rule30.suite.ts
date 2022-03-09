9842ee4cf824802d9a4524358e6f7d89a9628474
Adrien Belminksy <belminksy@gmail.com>
2022-03-10 00:04:26 +0100

    feat: add rule30 suite

---
+++ src/rule30.suite.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Executable, Path } from '@minimouli/framework'
+
+suite('Rule 30', () => {
+
+    test('Big', async () => {
+
+        const exec = new Executable('wolfram', [
+            '--rule', '30',
+            '--lines', '64'
+        ])
+
+        await exec.execute()
+
+        expect(exec).toExitWith(0)
+        await expect(exec).concurrent.toOutput(Path.fromMouli('/res/rule30/big'))
+    })
+
+    test('Bigger', async () => {
+
+        const exec = new Executable('wolfram', [
+            '--rule', '30',
+            '--lines', '128'
+        ])
+
+        await exec.execute()
+
+        expect(exec).toExitWith(0)
+        await expect(exec).concurrent.toOutput(Path.fromMouli('/res/rule30/bigger'))
+    })
+
+    test('With start', async () => {
+
+        const exec = new Executable('wolfram', [
+            '--rule', '30',
+            '--lines', '64',
+            '--start', '64'
+        ])
+
+        await exec.execute()
+
+        expect(exec).toExitWith(0)
+        await expect(exec).concurrent.toOutput(Path.fromMouli('/res/rule30/with-start'))
+    })
+
+})
