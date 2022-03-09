c85eec25e36ddf748b8d21bceb45911b60a28dd0
Adrien Belminksy <belminksy@gmail.com>
2022-03-10 00:05:21 +0100

    feat: add rule110 suite

---
+++ src/rule110.suite.ts
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) Minimouli
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { Executable, Path } from '@minimouli/framework'
+
+suite('Rule 110', () => {
+
+    test('Big', async () => {
+
+        const exec = new Executable('wolfram', [
+            '--rule', '110',
+            '--lines', '64'
+        ])
+
+        await exec.execute()
+
+        expect(exec).toExitWith(0)
+        await expect(exec).concurrent.toOutput(Path.fromMouli('/res/rule110/big'))
+    })
+
+    test('Bigger', async () => {
+
+        const exec = new Executable('wolfram', [
+            '--rule', '110',
+            '--lines', '128'
+        ])
+
+        await exec.execute()
+
+        expect(exec).toExitWith(0)
+        await expect(exec).concurrent.toOutput(Path.fromMouli('/res/rule110/bigger'))
+    })
+
+    test('With start', async () => {
+
+        const exec = new Executable('wolfram', [
+            '--rule', '110',
+            '--lines', '64',
+            '--start', '64'
+        ])
+
+        await exec.execute()
+
+        expect(exec).toExitWith(0)
+        await expect(exec).concurrent.toOutput(Path.fromMouli('/res/rule110/with-start'))
+    })
+
+})
